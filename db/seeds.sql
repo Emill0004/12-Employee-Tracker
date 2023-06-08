895dc699ad911b878a647e3fda6f222de865a8d0
Eric Miller <e.s.miller13125@gmail.com>
2023-06-08 08:35:01 -0600

    fixed bugs in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,16 +2,16 @@ INSERT INTO department (name)
 VALUES ('Legal'),
        ('Sales'),
        ('Finance'),
-       ('Engineering'),
+       ('Engineering');
 
 INSERT INTO role (title, salary)
 VALUES ('Salesperson', 75000.35),
        ('Accountant', 80000.35),
-       ('Lawyer', 999999999999.35),
-       ('Software Developer', 10.35),
+       ('Lawyer', 99999.35),
+       ('Software Developer', 10.35);
 
 INSERT INTO employee (first_name, last_name)
 VALUES ('Jimmy', 'Lawson'),
        ('Bob', 'Roberts'),
        ('Robert', 'Roberts'),
-       ('Kenzie', 'Smith'),
+       ('Kenzie', 'Smith');
